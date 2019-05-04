20abcdeef700c2d1f6d0b8e5055093be35cc4d21
Disco Turkey <gelsinon.gwen@gmail.com>
2019-05-03 19:31:10 -0600

    psudo code and some formulas

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+
+
+
+// player is shown a random number between 19-120
+var computerGuess = Math.floor(Math.random() * 121);
+
+//a number needs to be assigned to each crystal at start of game
+var aCrystalNum = Math.floor(Math.random() * 13);
+var bCrystalNum = Math.floor(Math.random() * 13);
+var cCrystalNum = Math.floor(Math.random() * 13);
+var dCrystalNum = Math.floor(Math.random() * 13);
+
+//if players total matches the number given then the game ends by
+var playerSum = 
+
+    //adding a win to their tally
+
+    //number tally resets
+
+    //new random number generated
+
+// if player's total exceeds random number shown the game ends
+
+    //number tally resets
+
+    //new random number generated
+
+    //add one to the loss tally
+
+// if players total is less than the random number then play continues
