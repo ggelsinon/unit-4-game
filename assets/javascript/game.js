7d21d4e24c093e0ec0d43cf86a339785b56dd838
Disco Turkey <gelsinon.gwen@gmail.com>
2019-05-12 21:16:39 -0600

    progress progress progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,53 @@
 
 
 
+//Game start events
 // player is shown a random number between 19-120
-var computerGuess = Math.floor(Math.random() * 121);
+var computerGuess = 0;
+
+var aCrystalNum = 0;
+var bCrystalNum = 0;
+var cCrystalNum = 0;
+var dCrystalNum = 0;
+
+var playerSum = 0;
+
+// vars for displaying information
+var winsText = document.getElementById("winsText");
+var lossesText = document.getElementById("lossesText");
+var runningTotal = document.getElementById("runningTotalTexts");
+var computerNum = document.getElementById("computerNumberText");
+
+
+
+$("#start").on("click", function() {
+// player is shown a random number between 19-120
+    computerNum = Math.floor(Math.random() * 121);
+    console.log(computerNum);
 
 //a number needs to be assigned to each crystal at start of game
-var aCrystalNum = Math.floor(Math.random() * 13);
-var bCrystalNum = Math.floor(Math.random() * 13);
-var cCrystalNum = Math.floor(Math.random() * 13);
-var dCrystalNum = Math.floor(Math.random() * 13);
+    rCrystalNum = Math.floor(Math.random() * 13);
+    bCrystalNum = Math.floor(Math.random() * 13);
+    yCrystalNum = Math.floor(Math.random() * 13);
+    gCrystalNum = Math.floor(Math.random() * 13);
+
+    computerNum.textContent = computerNum
+
+}
+
+
+)
+$(".crystal").on("click", function() {
+    $(this.dataNum)
+}
+
+)
+
+// jquery .attr
+
 
 //if players total matches the number given then the game ends by
-var playerSum = 
+var playerSum = 0;
 
     //adding a win to their tally
 
