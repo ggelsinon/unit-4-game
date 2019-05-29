77eb463e15824a386e1c8c69a396c77f2df36cf4
Disco Turkey <gelsinon.gwen@gmail.com>
2019-05-29 16:14:54 -0600

    bugs remaining in resetGame function, gemimg on click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,10 @@
 
-
-
-//Game start events
-// player is shown a random number between 19-120
-
-// global vars
 var computerNumber = 0;
 var playerSum = 0;
-var wins = 0
-var losses = 0
-
+var wins = 0;
+var losses = 0;
+var donzo = false;
+var winzo = false;
 
 var crystalObj = [
     {
@@ -34,83 +29,64 @@ var crystalObj = [
     },
 ];
 
-//make a random number variable so it can be used for all the random numbers that need to be generated with less code(thanks Genevieve for the tip)
 var rando = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//show the crystal objects on page load
+var winLossCheck = function () {
+    if (playerSum > computerNumber) {
+        donzo = true;
+    } else if (playerSum === computerNumber) {
+        winzo = true;
+        donzo = true;
+    }
+}
 
-// an event initiates the game
-// Genevieve tip #2 make start game a variable with a function assigned to it
-// this tip was super key to me figuring out where I was going wrong
-
-var start = function () {
-    computerNumber = rando(19, 120); // create a variable t
-
-    //a number needs to be assigned to each crystal object at start of game
+var setCrystals = function () {
     for (var i = 0; i < crystalObj.length; i++) {
         crystalObj[i].value = rando(1, 12);
         var img = $("<img>").attr('src', crystalObj[i].image).attr("id", "crystals" + (i + 1)).attr("data-index", i).addClass("gemimg button").attr("data-num", crystalObj[i].value);
-        // player is shown a random number between 19-120
-
-        // $(div).append(img)
         $("#crystals").append(img);
-
-
     }
-    // need all the place holders for our tally's to show up as part of the start game function too
-
-    $("#computerNumText").html(computerNumber);
-    $("#wins").html("Wins: " + wins);
-    $("#losses").html("Losses: " + losses);
-
-
 }
 
+var resetGame = function () {
+    computerNumber = 0;
+    playerSum = 0;
+    donzo = false;
+    winzo = false;
+    // crystalObj[i].value = 0;
+    $("#crystals").empty;
+    // setCrystals();
+}
+
+$("#winsText").html("Wins: " + wins);
+$("#lossesText").html("Losses: " + losses);
+
 $("#start").on("click", function () {
-    console.log("hi")
-    start();
+    console.log("start worked");
+    computerNumber = rando(19, 120);
+    $("#computerNumText").html("Number to match: " + computerNumber);
+    setCrystals();
+
+    $(".gemimg").on("click", function () {
+        winLossCheck()
+        if ((donzo = true) && (winzo = true)) {
+            wins += 1;
+            resetGame();
+            alert("You Win!")
+
+        } else if ((donzo = true) && (winzo = false)) {
+            losses += 1;
+            resetGame();
+            alert("Uh Oh! You lost!")
+
+        } else {
+            var crysvalue = $(this)[0].dataset.num;
+            playerSum += parseInt(crysvalue);
+            $("#runningTotalText").html("Player total: " + playerSum);
+        }
+    })
 
 })
 
-
-// needs to show up on the html
-$("#winsText").html("Your Wins: " + wins);
-$("#lossesText").html("Your Losses: " + losses);
-
-$("#crystals").on("click", ".gemimg", function () {
-    // $(this.dataNum)
-    var crysvalue = $(this)[0].dataset.num;
-    playerSum += parseInt (crysvalue);
-    $("#runningTotalText").html(playerSum);
-
-
-})
-// jquery .attr
-
-
-//this will be for the reset function
-// add crystal value to players running total
-
-// computerNumber = playerSum + crystalNum
-
-// //if players total matches the number given then the game ends by
-// playerSum = 0;
-
-//     //adding a win to their tally
-//     wins = wins++;
-
-// //number tally resets
-// playerSum = 0;
-// //new random number generated
-
-// // if player's total exceeds random number shown the game ends
-
-// //number tally resets
-// playerSum = 0;
-// //new random number generated
-
-// //add one to the loss tally
-// losses = losses++
-//     // if players total is less than the random number then play continues
